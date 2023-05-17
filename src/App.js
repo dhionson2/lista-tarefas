d93fb24f362c1627512f75f49870633427e49457
GRUPOMARISTA\dhionson.moreira <0899@@dh>
2023-05-16 22:37:02 -0300

    Finalização primeira etapa do projeto

--- src/App.js
+++ src/App.js
@@ -25,38 +25,51 @@ function ListaDeTarefas() {
 
   return (
     <div className="container">
-      <h2 className="titulo">Lista de Tarefas</h2>
-      <input
-        type="text"
-        value={novaTarefa}
-        onChange={(e) => setNovaTarefa(e.target.value)}
-        placeholder="Digite uma nova tarefa"
-        className="input-tarefa"
-      />
-      <button onClick={adicionarTarefa} className="botao-adicionar">
-        Adicionar Tarefa
-      </button>
-      <ul className="lista-tarefas">
-        {tarefas.map((tarefa, index) => (
-          <li key={index} className={index % 2 === 0 ? "linha-verde-claro" : "linha-verde-mais-claro"}>
-            <div className="linha-conteudo">
-              <input
-                type="checkbox"
-                checked={tarefa.concluida}
-                onChange={() => handleConcluirTarefa(index)}
-                className="checkbox-concluir"
-              />
-              <span className={tarefa.concluida ? "tarefa-concluida" : ""} style={{ marginLeft: '20px' }}>{tarefa.texto}</span>
-              {!tarefa.concluida && (
-                
-                <button onClick={() => removerTarefa(index)} className="botao-remover">
-                  Remover
-                </button>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
+      <h2 className="titulo-borda">Lista de Tarefas</h2>
+      <div className="borda-preta">
+        <input
+          type="text"
+          value={novaTarefa}
+          onChange={(e) => setNovaTarefa(e.target.value)}
+          placeholder="Digite uma nova tarefa"
+          className="input-tarefa"
+        />
+        <button onClick={adicionarTarefa} className="botao-adicionar">
+          Adicionar Tarefa
+        </button>
+        {tarefas.length > 0 && (
+          <table className="tabela-tarefas">
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tarefas.map((tarefa, index) => (
+                <tr key={index} className={index % 2 === 0 ? "linha-verde-claro" : "linha-verde-mais-claro"}>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={tarefa.concluida}
+                      onChange={() => handleConcluirTarefa(index)}
+                      className="checkbox-concluir"
+                    />
+                    <span className={tarefa.concluida ? "tarefa-concluida" : ""} style={{ marginLeft: '20px' }}>{tarefa.texto}</span>
+                  </td>
+                  <td>
+                    {!tarefa.concluida && (
+                      <button onClick={() => removerTarefa(index)} className="botao-remover">
+                        Remover
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
